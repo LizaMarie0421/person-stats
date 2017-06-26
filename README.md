631cad1de866a94df3df735cf00020a840db1cf2
LizaMarie <lizamariejimenez@gmail.com>
2017-06-26 13:51:45 -0500

    update input and change colors of text if color found

---
+++ README.md
@@ -0,0 +1,6 @@
+# person STats
+
+##Day 1 Homework
+update new _details_ div with the value of text input.
+#bonus
+* Add *change appearance of 'p' in css based on the value from the form. (ex turn text blue if user type blue in field.)
--- indez.js
+++ indez.js
@@ -1,9 +1,35 @@
 function updateHeading(ev){
-    ev.preventDefault()
-    const f =ev.target
-    const name = f.personName.value
-  const heading= document.querySelector('h1')
-  heading.innerHTML= name
+     ev.preventDefault()
+     const f =ev.target
+     const name = f.personName.value
+   const heading= document.querySelector('h1')
+   heading.innerHTML= "Person Stats"
+
+   const home = f.HomeTown.value
+   const div= document.querySelector('#div')
+   div.innerHTML= "My Name is "+ name+ " and I live in " + home
+ 
+if (home|| name === "blue"){
+    document.querySelector('#div').style.color="blue"
 }
-const personForm = document.querySelector('#person-form')
-personForm.addEventListener('submit', updateHeading)
+else if (home|| name === "red"){
+    document.querySelector('#div').style.color="red"
+}
+else if (home|| name === "green"){
+    document.querySelector('#div').style.color="green"
+}
+else if (home|| name === "yellow"){
+    document.querySelector('#div').style.color="yellow"
+}
+else if (home|| name === "orange"){
+    document.querySelector('#div').style.color="orange"
+}
+else if (home|| name === "pink"){
+    document.querySelector('#div').style.color="pink"
+}
+else{
+    document.querySelector('#div').style.color="black"
+}
+}
+ const personForm = document.querySelector('#person-form')
+ personForm.addEventListener('submit', updateHeading)
