196ecdf807e34764b5011755d70e4a49f9db5e3a
LizaMarie <lizamariejimenez@gmail.com>
2017-06-27 11:19:04 -0500

    added list items, color and age still limited on list ul list before homework

--- README.md
+++ README.md
@@ -4,3 +4,18 @@
 update new _details_ div with the value of text input.
 #bonus
 * Add *change appearance of 'p' in css based on the value from the form. (ex turn text blue if user type blue in field.)
+
+##day 2 
+*create new funtion called renderColor that return a div element
+* call  that function when adding that div to the colorItem
+#bonus
+* cr3eate a new function called renderListItem 
+* use it to create list items for each stat
+
+#bonusx2
+* create a new function called renderList
+*use it to create the list for each persons stats
+*call renderListItem from renderList, not directyly from handleSubmit
+##bonusx3
+*try not to use innerHTML. keep using doc.createelement
+
--- indez.js
+++ indez.js
@@ -1,25 +1,47 @@
-function updateHeading(ev){
+function handleSubmit(ev){
     ev.preventDefault()
     const f =ev.target
+    
     const name = f.personName.value
     const home = f.HomeTown.value
     const age = f.age.value
     const color = f.color.value
 
-   const div= document.querySelector('#stats')
+    const list= document.createElement('ul')
+
+    const nameItem= document.createElement('li')
+    nameItem.textContent =`Name: ${name}`
+    list.appendChild(nameItem)
+
+    const ageItem=document.createElement('li')
+    ageItem.textContent=`Age: ${age}`
+    list.appendChild(ageItem)
+
+    const colorItem=document.createElement('li')
+    colorItem.textContent=`Color:`
+
+    const colorDiv=document.createElement('li')
+    colorDiv.style.backgroundColor= color
+    colorDiv.style.width='6rem'
+    colorDiv.style.widht='3rem'
+    colorItem.appendChild(colorDiv)
+
+    const stats=document.querySelector('#stats')
+    stats.appendChild(list)
+   //const div= document.querySelector('#stats')
 /*   div.innerHTML= "My Name is "+ name+ " and I live in " + home +
-  ". I am"+age+ " years old and my favorite color is "+ color*/
+  ". I am"+age+ " years old and my favorite color is "+ color
   div.innerHTML= `
   <p style="color:${color}">
   My Name is ${name} and I live in ${home}.
   I am ${age} years old and my favorite color is${color}</p>
  `
   //screen interpulation
-/*const p= document.createElement('p')
+const p= document.createElement('p')
 p.textContent =`${name}, age${age}` 
 //doesnt exist on page yet until append
 stats.appendChild(p)*/
 
 }
  const personForm = document.querySelector('#person-form')
- personForm.addEventListener('submit', updateHeading)
+ personForm.addEventListener('submit', handleSubmit)
